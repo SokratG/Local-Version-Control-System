9b8120f356628b957743288bc7b838d1bf2735f2
Sokrat <39133972+SokratG@users.noreply.github.com>
2020-02-01 22:07:53 +0300

    Update stdafx.h

--- gitpow/include/stdafx.h
+++ gitpow/include/stdafx.h
@@ -11,11 +11,11 @@
 #define NOMINMAX
 #include "targetver.h"
 
-#define WIN32_LEAN_AND_MEAN             // Исключите редко используемые компоненты из заголовков Windows
-// Файлы заголовков Windows
+#define WIN32_LEAN_AND_MEAN             
+
 #include <windows.h>
 #include <shlobj.h> 
-// Файлы заголовков среды выполнения C
+
 #include <stdlib.h>
 #include <malloc.h>
 #include <memory.h>
